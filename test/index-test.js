71d265dcbcde980baabc8d103fd37dc4f08b05c9
Miyuki Abe <miyuki0809@gmail.com>
2018-04-14 18:54:25 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,8 +5,6 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
-a += b
-
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
 })
